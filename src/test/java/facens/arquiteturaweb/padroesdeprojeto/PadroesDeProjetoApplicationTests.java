51f6f1ea575c37eeb79916bd8d78c8385ce94406
Amauri Antunes Filho <amauri.antunesfilho@gmail.com>
2024-01-10 19:16:34 -0300

    first commit

---
+++ src/test/java/facens/arquiteturaweb/padroesdeprojeto/PadroesDeProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package facens.arquiteturaweb.padroesdeprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesDeProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
